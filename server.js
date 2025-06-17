6000bd6766c6be673dc6c75be20e0599bc7342a8
fgaisboeck <Gaisboeckf190055@sus.htl-grieskirchen.at>
2025-06-17 20:50:43 +0200

    ready to railway

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Statische Dateien servieren
+app.use(express.static(__dirname));
+
+// Route für die Hauptseite
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Route für mehr-info.html
+app.get('/mehr-info', (req, res) => {
+    res.sendFile(path.join(__dirname, 'mehr-info.html'));
+});
+
+// Server starten
+app.listen(port, () => {
+    console.log(`Server läuft auf Port ${port}`);
+}); 
